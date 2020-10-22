41262e4d8dc27e8fa3e05d96ddcadb72aefd025a
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-22 15:06:20 +0200

    Added realtime logic for sending stock information

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const db = require('./db/database.js');
+const ObjectId = require('mongodb').ObjectID;
+
+const port = process.env.SOCKET_PORT || 3001;
+
+let stocks = {};
+
+(async () => {
+    await db.connect();
+
+    app.use(cors());
+    io.origins('*:*');
+
+    const collection = db.client().db('project').collection('stocks');
+
+    collection.find().toArray((err, res) => {
+        if (err) {
+            return;
+        }
+
+        res.forEach((stock) => {
+            stocks[stock._id] = stock.value;
+        });
+    });
+
+    io.on('connection', (socket) => {
+        socket.on('stock connect', (message) => {
+            collection.findOne({ _id: new ObjectId(message.id) }, (err, stock) => {
+                if (err) {
+                    return;
+                }
+
+                if (!stock) {
+                    return;
+                }
+
+                if (message.latest !== stock.value[stock.value.length - 1].date) {
+                    socket.emit('stock correction', { id: message.id, value: stock.value });
+                }
+            });
+        });
+    });
+
+    setInterval(() => {
+        for (const key in stocks) {
+            if (stocks[key]) {
+                const value = stocks[key];
+
+                const data = {
+                    date: new Date().toISOString(),
+                    value: value[value.length - 1].value + (Math.random() > 0.5 ? 1 : -1)
+                };
+
+                stocks[key].push(data);
+
+                collection.updateOne({ _id: new ObjectId(key) }, {
+                    $push: {
+                        value: data
+                    }
+                }, (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+
+                io.emit('stock update', { id: key, value: data });
+            }
+        }
+    }, 1000);
+
+    server.listen(port, () => {
+        console.log(`Running socket server on port ${port}`);
+    });
+})();
