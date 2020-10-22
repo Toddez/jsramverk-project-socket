349bd202b5e1b919fe7c9fff5ebd804ac7e98d6d
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-22 15:06:02 +0200

    Added database module

---
+++ db/database.js
@@ -0,0 +1,34 @@
+const mongodb = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+
+class Database {
+    static async connect() {
+        await new Promise(resolve => {
+            mongodb.MongoClient.connect(
+                url,
+                {
+                    useUnifiedTopology: true
+                },
+                (err, client) => {
+                    if (err)
+                        return;
+
+                    Database.mongoClient = client;
+                    resolve();
+                }
+            );
+        });
+
+        return Database;
+    }
+
+    static client() {
+        if (!Database.mongoClient)
+            return;
+
+        return Database.mongoClient;
+    }
+}
+
+module.exports = Database;
